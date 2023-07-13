6634e66ab4384bd58acf20b72f08d69fc1b1ade7
Petar Todorovski <petar.todorovski007@gmail.com>
2023-07-13 21:00:19 +0200

    simplification of simulation app

--- app.r
+++ app.r
@@ -58,18 +58,16 @@ ui <- fluidPage(
 )
 
 server <- function(input, output, session) {
+  x1 <- reactive(rnorm(input$n1, input$mean1, input$sd1))
+  x2 <- reactive(rnorm(input$n2, input$mean2, input$sd2))
+  
   output$hist <- renderPlot({
-    x1 <- rnorm(input$n1, input$mean1, input$sd1)
-    x2 <- rnorm(input$n2, input$mean2, input$sd2)
-    
-    freqpoly(x1, x2, binwidth = input$binwidth, xlim = input$range)
+    freqpoly(x1(), x2(), binwidth = input$binwidth, xlim = input$range)
   }, res = 96)
   
   output$ttest <- renderText({
-    x1 <- rnorm(input$n1, input$mean1, input$sd1)
-    x2 <- rnorm(input$n2, input$mean2, input$sd2)
-    
-    t_test(x1, x2)
+    t_test(x1(), x2())
   })
+
 }
 shinyApp(ui, server)
