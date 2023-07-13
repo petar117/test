ab0aa3ebec82698b3b1c45bbfb746f7e16aed284
Petar Todorovski <petar.todorovski007@gmail.com>
2023-07-13 17:13:42 +0200

    product

--- app.r
+++ app.r
@@ -1,22 +1,14 @@
 library(shiny)
 
 ui <- fluidPage(
-  selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
-  verbatimTextOutput("summary"),
-  tableOutput("table")
+  sliderInput("x", label = "If x is", min = 1, max = 50, value = 30),
+  "then x times 5 is",
+  textOutput("product")
 )
 
 server <- function(input, output, session) {
-  dataset <- reactive({
-    get(input$dataset, "package:datasets")
-  })
-  
-  output$summary <- renderPrint({
-    summary(dataset())
-  })
-  
-  output$table <- renderTable({
-    dataset()
+  output$product <- renderText({ 
+    input$x * 5
   })
 }
 
