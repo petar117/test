2774755b2d4336550a8bb05fe7ff7cd336fb0322
Petar Todorovski <petar.todorovski007@gmail.com>
2023-07-17 22:07:27 +0200

    finished er basics

--- app.r
+++ app.r
@@ -57,19 +57,34 @@ prod_codes <- setNames(products$prod_code, products$title)
 
 ui <- fluidPage(
   fluidRow(
-    
+    column(6,
+           selectInput("code", "Product", choices = prod_codes)
+    )
   ),
   fluidRow(
-    
+    column(4, tableOutput("diag")),
+    column(4, tableOutput("body_part")),
+    column(4, tableOutput("location"))
   ),
   fluidRow(
-    
+    column(12, plotOutput("age_sex"))
   )
-  
 )
 
 server <- function(input, output, session) {
+  selected <- reactive(injuries %>% filter(prod_code == input$code))
   
+  output$diag <- renderTable(
+    selected() %>% count(diag, wt = weight, sort = TRUE)
+    )
+  
+  output$body_part <- renderTable(
+    selected() %>% count(body_part, wt = weight, sort = TRUE)
+  )
+  
+  output$location <- renderTable(
+    selected() %>% count(location, wt = weight, sort = TRUE)
+  )
 }
 
 shinyApp(ui, server)
