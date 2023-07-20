9df54930920c33763c045d271e08c5e2bf40d690
Petar Todorovski <petar.todorovski007@gmail.com>
2023-07-20 16:24:24 +0200

    adding a panel

--- pt2/app.R
+++ pt2/app.R
@@ -1,11 +1,23 @@
 library(shiny)
+library(glue)
 
-ui <- fluidPage(
-  sliderInput("x", "x", value = 1, min = 0, max = 10),
-  sliderInput("y", "y", value = 2, min = 0, max = 10),
-  sliderInput("z", "z", value = 3, min = 0, max = 10),
-  textOutput("total")
+ui <- navbarPage(
+  "Page title",   
+  tabPanel("panel 1", "one", 
+           sliderInput("x", "x", value = 1, min = 0, max = 10),
+           sliderInput("y", "y", value = 2, min = 0, max = 10),
+           sliderInput("z", "z", value = 3, min = 0, max = 10),
+           textOutput("total")),
+  tabPanel("panel 2", "two"),
+  tabPanel("panel 3", "three"),
+  navbarMenu("subpanels", 
+             tabPanel("panel 4a", "four-a"),
+             tabPanel("panel 4b", "four-b"),
+             tabPanel("panel 4c", "four-c")
+  )
 )
+
+
 server <- function(input, output, session) {
   observeEvent(input$x, {
     message(glue("Updating y from {input$y} to {input$x * 2}"))
