203f6a374ce04b2b0ec8b1c8ed57762e64424b4b
Petar Todorovski <petar.todorovski007@gmail.com>
2023-07-20 16:53:30 +0200

    adding theme

--- pt2/app.R
+++ pt2/app.R
@@ -2,18 +2,28 @@ library(shiny)
 library(glue)
 
 ui <- navbarPage(
-  "Page title",   
-  tabPanel("panel 1", "one", 
-           sliderInput("x", "x", value = 1, min = 0, max = 10),
-           sliderInput("y", "y", value = 2, min = 0, max = 10),
-           sliderInput("z", "z", value = 3, min = 0, max = 10),
-           textOutput("total")),
-  tabPanel("panel 2", "two"),
+  theme = bslib::bs_theme(bootswatch = "sandstone"),
+  "Page title",
+  tabPanel(
+    "panel 1", "one",
+    sliderInput("x", "x", value = 1, min = 0, max = 10),
+    sliderInput("y", "y", value = 2, min = 0, max = 10),
+    sliderInput("z", "z", value = 3, min = 0, max = 10),
+    textOutput("total")
+  ),
+  tabPanel(
+    "panel 2", "two",
+    fileInput("file", "Data", buttonLabel = "Upload..."),
+    textInput("delim", "Delimiter (leave blank to guess)", ""),
+    numericInput("skip", "Rows to skip", 0, min = 0),
+    numericInput("rows", "Rows to preview", 10, min = 1)
+  ),
   tabPanel("panel 3", "three"),
-  navbarMenu("subpanels", 
-             tabPanel("panel 4a", "four-a"),
-             tabPanel("panel 4b", "four-b"),
-             tabPanel("panel 4c", "four-c")
+  navbarMenu(
+    "subpanels",
+    tabPanel("panel 4a", "four-a"),
+    tabPanel("panel 4b", "four-b"),
+    tabPanel("panel 4c", "four-c")
   )
 )
 
