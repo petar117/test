76e8c3b85a22f32676864cf877ede38e889a4d2a
Petar Todorovski <petar.todorovski007@gmail.com>
2023-07-24 23:01:37 +0200

    using tabsetPanel for onditional UI

--- pt2/dynamic-ui/app.R
+++ pt2/dynamic-ui/app.R
@@ -8,6 +8,23 @@ sales %>%
   arrange(ORDERNUMBER)
 
 
+parameter_tabs <- tabsetPanel(
+  id = "params",
+  type = "hidden",
+  tabPanel("normal",
+           numericInput("mean", "mean", value = 1),
+           numericInput("sd", "standard deviation", min = 0, value = 1)
+  ),
+  tabPanel("uniform", 
+           numericInput("min", "min", value = 0),
+           numericInput("max", "max", value = 1)
+  ),
+  tabPanel("exponential",
+           numericInput("rate", "rate", value = 1, min = 0),
+  )
+)
+
+
 ui <- navbarPage(
   "Page title",
   tabPanel(
@@ -120,7 +137,23 @@ ui <- navbarPage(
           )
         )
       )
+    ),
+    fluidRow(HTML("<hr>")),
+    fluidPage(
+      sidebarLayout(
+        sidebarPanel(
+          selectInput("dist", "Distribution", 
+                      choices = c("normal", "uniform", "exponential")
+          ),
+          numericInput("n5", "Number of samples", value = 100),
+          parameter_tabs,
+        ),
+        mainPanel(
+          plotOutput("hist")
+        )
+      )
     )
+    
   )
 )
 server <- function(input, output, session) {
@@ -250,6 +283,18 @@ server <- function(input, output, session) {
     updateTabsetPanel(inputId = "switcher", selected = input$controller)
   })
   
+  observeEvent(input$dist, {
+    updateTabsetPanel(inputId = "params", selected = input$dist)
+  }) 
+  
+  sample <- reactive({
+    switch(input$dist,
+           normal = rnorm(input$n5, input$mean, input$sd),
+           uniform = runif(input$n5, input$min, input$max),
+           exponential = rexp(input$n5, input$rate)
+    )
+  })
+  output$hist <- renderPlot(hist(sample()), res = 96)
 }
 
 shinyApp(ui, server)
