46507c4d985e88f1546aa831ddf601a7e17ed40e
Petar Todorovski <petar.todorovski007@gmail.com>
2023-07-25 23:48:07 +0200

    commit

--- pt2/dynamic-ui/app2.R
+++ pt2/dynamic-ui/app2.R
@@ -1,5 +1,30 @@
 library(shiny)
 
+make_ui <- function(x, var) {
+  if (is.numeric(x)) {
+    rng <- range(x, na.rm = TRUE)
+    sliderInput(var, var, min = rng[1], max = rng[2], value = rng)
+  } else if (is.factor(x)) {
+    levs <- levels(x)
+    selectInput(var, var, choices = levs, selected = levs, multiple = TRUE)
+  } else {
+    # Not supported
+    NULL
+  }
+}
+
+filter_var <- function(x, val) {
+  if (is.numeric(x)) {
+    !is.na(x) & x >= val[1] & x <= val[2]
+  } else if (is.factor(x)) {
+    x %in% val
+  } else {
+    # No control, so don't filter
+    TRUE
+  }
+}
+
+
 ui <- navbarPage(
   "Page title",
   tabPanel(
@@ -20,7 +45,34 @@ ui <- navbarPage(
           plotOutput("plot")  
         )
       )
+    ),
+    fluidRow(HTML("<hr>")),
+    fluidPage(
+      sidebarLayout(
+        sidebarPanel(
+          make_ui(iris$Sepal.Length, "Sepal.Length"),
+          make_ui(iris$Sepal.Width, "Sepal.Width"),
+          make_ui(iris$Species, "Species")
+        ),
+        mainPanel(
+          tableOutput("data")
+        )
+      )
+    ),
+    fluidRow(HTML("<hr>")),
+    fluidPage(
+      sidebarLayout(
+        sidebarPanel(
+          map(names(iris), ~ make_ui(iris[[.x]], .x))
+        ),
+        mainPanel(
+          tableOutput("data1")
+        )
+      )
     )
+  ),
+  tabPanel(
+    "tab 2",
     
   )
   
@@ -56,6 +108,24 @@ server <- function(input, output, session) {
       axes = FALSE
     )
   }, res = 96)
+  
+  
+  
+  selected <- reactive({
+    filter_var(iris$Sepal.Length, input$Sepal.Length) &
+      filter_var(iris$Sepal.Width, input$Sepal.Width) &
+      filter_var(iris$Species, input$Species)
+  })
+  
+  output$data <- renderTable(head(iris[selected(), ], 12))
+  
+  
+  selected1 <- reactive({
+    each_var <- map(names(iris), ~ filter_var(iris[[.x]], input[[.x]]))
+    reduce(each_var, ~ .x & .y)
+  })
+  
+  output$data1 <- renderTable(head(iris[selected1(), ], 12))
 }
 
 shinyApp(ui, server)
