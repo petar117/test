95e2189fcbd9fb69038fe270f5e46852b9b0defa
Petar Todorovski <petar.todorovski007@gmail.com>
2023-07-31 01:25:39 +0200

    commit

--- pt2/testing.R
+++ pt2/testing.R
@@ -7,6 +7,7 @@ library(tidyverse)
 library(tidytext)
 library(sentimentr)
 library(textstem)
+library(shiny)
 
 
 
@@ -47,5 +48,26 @@ data1 <- text_df %>%
   data1 %>% head(100) %>% 
   wordcloud2()
 
+  
+  ui <- fluidPage(
+    textInput("name", "name"),
+    actionButton("add", "add"),
+    actionButton("del", "delete"),
+    textOutput("names")
+  )
+  server <- function(input, output, session) {
+    r <- reactiveValues(names = character())
+    observeEvent(input$add, {
+      r$names <- union(r$names, input$name)
+      updateTextInput(session, "name", value = "")
+    })
+    observeEvent(input$del, {
+      r$names <- setdiff(r$names, input$name)
+      updateTextInput(session, "name", value = "")
+    })
+    
+    output$names <- renderText(r$names)
+  }
 
- 
+  shinyApp(ui = ui, server = server)
+  
